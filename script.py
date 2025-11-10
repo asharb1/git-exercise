14aca2c8d76dfc21f7e3d06be2655336af63ac2e
asharb1 <asharb1@umbc.edu>
2025-11-09 23:58:44 -0500

    Add files via upload

--- script.py
+++ script.py
@@ -2,3 +2,17 @@ def hello():
     print("hello")
 
 hello()
+
+git add script.py
+git commit -m "Add hello function"
+
+
+def goodbye():
+    print("goodbye")
+
+goodbye()
+
+git add script.py
+git commit -m "Add goodbye function"
+
+
